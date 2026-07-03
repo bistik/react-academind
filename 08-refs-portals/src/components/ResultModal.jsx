dd4fef3650a4d107efe79cd3cdc5816b956129a9
bistik <erickbianes@gmail.com>
2026-07-03 09:47:15 +0800

    wrap up almost-time-up game app

---
+++ 08-refs-portals/src/components/ResultModal.jsx
@@ -0,0 +1,39 @@
+import { useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export default function ResultModal({
+  result,
+  ref,
+  targetTime,
+  remainingTime,
+  onReset,
+}) {
+  const dialog = useRef();
+  const playerLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+  return createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {playerLost && <h2>You lost</h2>}
+      {!playerLost && <h2>Your score: {score}</h2>}
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
+      </p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal"),
+  );
+}
